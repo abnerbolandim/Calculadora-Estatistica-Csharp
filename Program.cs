2bc0e318d6e5d68ab681a7054483d0f73c1e2091
Abner <contato.abnerbolandim@gmail.com>
2023-07-04 10:14:20 -0300

    Adiciona método para calculo da estatistica

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculadora
+{
+    public class Statistics
+    {
+        public double Average{get; set;}
+        public double Median{get; set;}
+        public double Common{get; set;}
+        public double Deviation{get; set;}
+        public double Minimum{get; set;}
+        public double Maximum{get; set;}  
+    }
+
+    public class StatisticsCalculator
+    {
+        public Statistics CalculateStatistics(List<double> numbers)
+        {
+            Statistics statistics = new Statistics();
+            
+            if(numbers.Count > 0)
+            {
+                statistics.Average = numbers.Average();
+                statistics.Median = numbers.Min();
+                statistics.Common= numbers.Max();
+            }
+
+            return statistics;
+        } 
+    }
+}
