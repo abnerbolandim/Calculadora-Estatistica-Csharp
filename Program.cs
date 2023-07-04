3335c32ed3c56860f732f33b2845d2f41a1a5cfb
Abner <contato.abnerbolandim@gmail.com>
2023-07-04 11:37:03 -0300

    Adiciona método correspondente à achar a mediana

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculadora
     public class Statistics
     {
         public double Average{get; set;}
-        public double Median{get; set;}
+        public int Median{get; set;}
         public double Common{get; set;}
         public double Deviation{get; set;}
         public double Minimum{get; set;}
@@ -22,11 +22,40 @@ namespace Calculadora
             if(numbers.Count > 0)
             {
                 statistics.Average = numbers.Average();
-                statistics.Median = numbers.Min();
-                statistics.Common= numbers.Max();
+                statistics.Minimum = numbers.Min();
+                statistics.Maximum =  numbers.Max();
             }
 
-            return statistics;
+            return statistics;  
         } 
+        
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static double GetMedian(List<int> numbers)
+        {
+            numbers.Sort();
+
+            double median;
+            int middleIndex = numbers.Count / 2;
+
+            if(numbers.Count % 2 == 0)
+            {
+                median = (numbers[middleIndex - 1] + numbers[middleIndex]) / 2.0;  
+            }
+
+            else
+            {
+                median = numbers[middleIndex];
+            }
+
+            return median;
+        }
     }
 }
